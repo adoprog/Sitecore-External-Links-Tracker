9dbca905a9e789f3bcbbaa8b986e23945550179e
Alexander Doroshenko <adoprog@gmail.com>
2012-10-23 14:12:55 +0300

    Files copied from TRAC

---
+++ LinksTracker.svc.cs
@@ -0,0 +1,31 @@
+﻿namespace Analytics.ExternalLinksTracker
+{
+    using System.ServiceModel;
+    using System.ServiceModel.Activation;
+    using Sitecore.Analytics;
+
+    /// <summary>
+    /// Defines the links tracker class.
+    /// </summary>
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class LinksTracker
+    {
+        /// <summary>
+        /// Tracks the outgoing URL.
+        /// </summary>
+        /// <param name="url">The URL that should be stored.</param>
+        [OperationContract]
+        public void TrackExternalLink(string url)
+        {
+            AnalyticsTracker.StartTracking();
+            var tracker = AnalyticsTracker.Current;
+            if (tracker != null && tracker.PreviousPage != null && tracker.CurrentPage != null)
+            {
+                tracker.PreviousPage.TriggerEvent("External Link", url);
+                tracker.CurrentPage.Url = url;
+                tracker.Submit();
+            }
+        }
+    }
+}
---
+++ js/LinksTracker.js
@@ -0,0 +1,15 @@
+﻿jQuery(document).ready(function() {
+    jQuery.expr[':'].external = function(obj) {
+        return !obj.href.match(/^mailto\:/)
+                && (obj.hostname != location.hostname);
+    };
+    jQuery("a:external").click(function() {
+        jQuery.ajax({
+            type: "POST",
+            url: "/sitecore modules/web/ExternalLinksTracker/LinksTracker.svc/TrackExternalLink",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: '{"url": "' + jQuery(this).attr('href') + '"}'
+        });
+    });    
+});
